7bd1854b0bf37ebbbdff23a8c41d86a5824ad818
fmDevart <fm.devart@gmail.com>
2023-06-18 18:46:21 +0200

    first commit

---
+++ src/main/java/com/fmdevar/springboot/learnspringboot/Course.java
@@ -0,0 +1,37 @@
+package com.fmdevar.springboot.learnspringboot;
+
+public class Course {
+    private long id;
+    private String title;
+    private String author;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Course(long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+}
---
+++ src/main/java/com/fmdevar/springboot/learnspringboot/CurrencyServiceConfiguration.java
@@ -0,0 +1,38 @@
+package com.fmdevar.springboot.learnspringboot;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "currency-service")
+@Component
+
+public class CurrencyServiceConfiguration {
+
+    public String name;
+    public String url;
+    public String key;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
